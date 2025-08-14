d86b2535ac935e8c0f63e1c79c83ff297ad72c6e
Electron7-7 <julian@frattarola.net>
2025-08-14 17:44:51 -0400

    refactor(breaking): Moved `CompareStrings` into its own header file

--- src/getargs/argument.hpp
+++ src/getargs/argument.hpp
@@ -1,29 +1,6 @@
 #ifndef ARGUMENT_H
 #define ARGUMENT_H
 
-constexpr bool CompareStrings(const char* Left, const char* Right)
-{
-    if(Left == Right) // Hey, it could happen
-        return true;
-
-    if(Left == nullptr || Right == nullptr) // All my homies HATE segfaults!!!!
-        return false;
-
-    while(*Left != '\0' && *Right != '\0')
-    {
-        if(*Left != *Right)
-            return false;
-
-        Left++;
-        Right++;
-    }
-
-    if(*Left != '\0' || *Right != '\0') // One of them is shorter than the other!
-        return false;
-
-    return true;
-}
-
 struct _Arg
 {
 public:
